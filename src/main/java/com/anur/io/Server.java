a4d2bc54718df134c194a17b9b17cf44e9e77bd5
Anur <anurnomeru@163.com>
2019-01-18 18:16:32 +0800

    -

---
+++ src/main/java/com/anur/io/Server.java
@@ -0,0 +1,56 @@
+package com.anur.io;
+
+import java.net.InetSocketAddress;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+/**
+ * Created by Anur IjuoKaruKas on 2019/1/18
+ *
+ * 供其他端连接的套接字服务端入口
+ */
+public class Server {
+
+    private final int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = Integer.parseInt(args[0]);
+        new Server(port).start();
+    }
+
+    public void start() throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(group)
+                           .channel(NioServerSocketChannel.class)
+                           .localAddress(new InetSocketAddress(port))
+                           .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                               @Override
+                               protected void initChannel(SocketChannel socketChannel) {
+                                   socketChannel.pipeline()
+                                                .addLast(serverHandler);
+                               }
+                           });
+
+            ChannelFuture f = serverBootstrap.bind()
+                                             .sync();
+            f.channel()
+             .closeFuture()
+             .sync();
+        } finally {
+            group.shutdownGracefully()
+                 .sync();
+        }
+    }
+}
---
+++ src/main/java/com/anur/io/handler/InitialHandler.java
@@ -0,0 +1,13 @@
+package com.anur.io.handler;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+/**
+ * Created by Anur IjuoKaruKas on 2019/1/18
+ */
+public class InitialHandler extends ChannelInitializer<SocketChannel> {
+
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        socketChannel
+    }
+}
