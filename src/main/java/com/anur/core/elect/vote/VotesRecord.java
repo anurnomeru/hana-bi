8f085393534b6ba8bd52782c6a7e1a4e6191fb7b
Anur <anurnomeru@163.com>
2019-01-22 16:36:46 +0800

    投票机制完善

--- src/main/java/com/anur/core/elect/vote/VotesRecord.java
+++ src/main/java/com/anur/core/elect/vote/VotesRecord.java
@@ -1,6 +1,8 @@
 package com.anur.core.elect.vote;
 
+import com.anur.core.elect.vote.model.Votes;
 import com.anur.core.lock.ReentrantLocker;
+
 /**
  * Created by Anur IjuoKaruKas on 2019/1/22
  *
@@ -31,6 +33,27 @@ public class VotesRecord extends ReentrantLocker {
                 this.generation = generation;
                 this.votedServerName = null;
                 return true;
+            }
+            return false;
+        });
+    }
+
+    /**
+     * 给某个服务投票，投票成功则返回true
+     * 1、新的一轮投票（generation更大），返回true
+     * 2、当前一轮的投票，如果投的是同一个server，返回true
+     */
+    public boolean vote(Votes votes) {
+        return this.lockSupplier(() -> {
+            if (votes.getGeneration() > this.generation) {
+                this.initVotesRecord(votes.getGeneration());
+                this.votedServerName = votes.getServerName();
+                return true;
+            } else if (votes.getGeneration() == this.generation) {
+                if (votes.getServerName()
+                         .equals(votedServerName)) {
+                    return true;
+                }
             }
             return false;
         });
