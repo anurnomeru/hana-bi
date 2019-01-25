5ae421a8c2dc1eb8dd6d5c761b026fade096f5ce
Anur <anurnomeru@163.com>
2019-01-25 09:46:02 +0800

    初始化提交简单的编解码器

--- src/main/java/com/anur/core/coder/Coder.java
+++ src/main/java/com/anur/core/coder/Coder.java
@@ -1,10 +1,7 @@
 package com.anur.core.coder;
 
 import java.util.Optional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.alibaba.fastjson.JSON;
-import com.anur.core.elect.vote.model.Votes;
 import com.anur.exception.HanabiException;
 import io.netty.util.internal.StringUtil;
 
@@ -15,25 +12,11 @@ public class Coder {
 
     private static final String REGEX = "-";
 
-    private static Logger logger = LoggerFactory.getLogger(Coder.class);
-
-    public static void main(String[] args) {
-        Votes votes = new Votes();
-        votes.setServerName("asdfasdfasdf");
-        votes.setGeneration(111);
-
-        String encode = Coder.encode(ProtocolEnum.VOTE, votes);
-
-        Votes votes111 = Coder.decode(encode);
-
-        System.out.println();
-    }
-
     public static String encode(ProtocolEnum protocolEnum, Object obj) {
         return protocolEnum.name() + REGEX + JSON.toJSONString(obj);
     }
 
-    public static <T> T decode(String str) {
+    public static DecodeWrapper decode(String str) {
         if (StringUtil.isNullOrEmpty(str)) {
             throw new DecodeException("解码失败，从其他节点收到的请求为空：" + str);
         }
@@ -45,12 +28,22 @@ public class Coder {
                                             .map(ProtocolEnum::valueOf)
                                             .orElseThrow(() -> new DecodeException("解码失败，从其他节点收到的请求有误：" + str));
 
-        Object obj = Optional.of(str)
-                             .map(s -> s.substring(regexIndex + 1))
-                             .map(s -> JSON.parseObject(s, protocolEnum.clazz))
-                             .orElseThrow(() -> new DecodeException("解码失败，从其他节点收到的请求有误：" + str));
+        return new DecodeWrapper(protocolEnum, Optional.of(str)
+                                                       .map(s -> s.substring(regexIndex + 1))
+                                                       .map(s -> JSON.parseObject(s, protocolEnum.clazz))
+                                                       .orElseThrow(() -> new DecodeException("解码失败，从其他节点收到的请求有误：" + str)));
+    }
 
-        return (T) obj;
+    public static class DecodeWrapper<T> {
+
+        public ProtocolEnum protocolEnum;
+
+        public T t;
+
+        public DecodeWrapper(ProtocolEnum protocolEnum, T t) {
+            this.protocolEnum = protocolEnum;
+            this.t = t;
+        }
     }
 
     public static class DecodeException extends HanabiException {
