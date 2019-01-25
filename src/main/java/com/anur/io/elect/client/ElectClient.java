2bc70d5d3af0664dd77381bb5628e75bf243b4ef
Anur <anurnomeru@163.com>
2019-01-25 20:34:07 +0800

    从博客抄了一套感觉很不靠谱的断线重连机制，待观察和改进

--- src/main/java/com/anur/io/elect/client/ElectClient.java
+++ src/main/java/com/anur/io/elect/client/ElectClient.java
@@ -1,6 +1,10 @@
 package com.anur.io.elect.client;
 
-import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -11,30 +15,39 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 
 /**
  * Created by Anur IjuoKaruKas on 2019/1/19
+ *
+ * 负责连接其他的服务器
  */
 public class ElectClient {
+
+    private static List<EventLoopGroup> cc = new ArrayList<>();
+
+    private final String serverName;
 
     private final String host;
 
     private final int port;
 
+    private Logger logger = LoggerFactory.getLogger(ElectClient.class);
+
     public static void main(String[] args) throws Exception {
-        new ElectClient("localhost", 10000).start();
+        new ElectClient("hanabi_test", "localhost", 10000).start();
     }
 
-    public ElectClient(String host, int port) {
+    public ElectClient(String serverName, String host, int port) {
+        this.serverName = serverName;
         this.host = host;
         this.port = port;
     }
 
-    public void start() throws Exception {
+    public void start() {
         EventLoopGroup eventExecutors = new NioEventLoopGroup();
+        cc.add(eventExecutors);
 
         try {
             Bootstrap bootstrap = new Bootstrap();
             bootstrap.group(eventExecutors)
                      .channel(NioSocketChannel.class)
-                     .remoteAddress(new InetSocketAddress(host, port))
                      .handler(new ChannelInitializer<SocketChannel>() {
 
                          @Override
@@ -44,14 +57,29 @@ public class ElectClient {
                          }
                      });
 
-            ChannelFuture channelFuture = bootstrap.connect()
-                                                   .sync();
+            ChannelFuture channelFuture = bootstrap.connect(host, port);
+            channelFuture.addListener(future -> {
+                if (!future.isSuccess()) {
+                    logger.info("与节点 {} [{}:{}] 的连接异常，正在重连 ...", serverName, host, port);
+                    ((ChannelFuture) future).channel()
+                                            .eventLoop()
+                                            .schedule(() -> new ElectClient(serverName, host, port).start(), 1L, TimeUnit.SECONDS
+                                            );
+                }
+            });
+
             channelFuture.channel()
                          .closeFuture()
                          .sync();
-        } catch (InterruptedException e) {
-            eventExecutors.shutdownGracefully()
-                          .sync();
+        } catch (
+
+            InterruptedException e) {
+            try {
+                eventExecutors.shutdownGracefully()
+                              .sync();
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
         }
     }
 }
