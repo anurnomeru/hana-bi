490c2f6bee856ed92d1b67a8f3ec9bbda1963ee2
Anur <anurnomeru@163.com>
2019-03-27 23:15:30 +0800

    1、新增一致性控制器 2、将协调器控制代码转移到一致性控制器中

--- src/main/java/com/anur/core/coordinate/model/Response.java
+++ src/main/java/com/anur/core/coordinate/model/Response.java
@@ -2,16 +2,23 @@ package com.anur.core.coordinate.model;
 
 import java.nio.ByteBuffer;
 import java.util.function.Consumer;
+
 /**
  * Created by Anur IjuoKaruKas on 2019/3/27
  *
- * 消息发送后的回包
+ * 消息发送后的回调
  */
 public class Response {
 
-    Consumer<ByteBuffer> callBack;
+    private boolean complate;
+
+    private Consumer<ByteBuffer> callBack;
 
     public Response(Consumer<ByteBuffer> callBack) {
         this.callBack = callBack;
+    }
+
+    public boolean isComplate() {
+        return complate;
     }
 }
--- src/main/java/com/anur/core/util/InFlightRequestManager.java
+++ src/main/java/com/anur/core/util/InFlightRequestManager.java
@@ -1,7 +1,15 @@
 package com.anur.core.util;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.anur.core.coordinate.model.Response;
+import com.anur.core.lock.ReentrantLocker;
+import com.anur.core.util.ChannelManager.ChannelType;
 import com.anur.io.store.common.Operation;
 import com.anur.io.store.common.OperationTypeEnum;
 
@@ -13,11 +21,42 @@ import com.anur.io.store.common.OperationTypeEnum;
  * 1、消息在没有收到回复之前，会定时重发。
  * 2、那么如何保证数据不被重复消费：我们以时间戳作为 key 的一部分，应答方需要在消费消息后，需要记录此时间戳，并不再消费比此时间戳小的消息。
  */
-public class InFlightRequestManager {
+public class InFlightRequestManager extends ReentrantLocker {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InFlightRequestManager.class);
 
     private Map<String, Set<OperationTypeEnum>> inFlight;
 
-    public void send(String serverName, Operation operation) {
+    public boolean send(String serverName, Operation operation, Response response) {
         OperationTypeEnum typeEnum = operation.getOperationTypeEnum();
+        return this.lockSupplier(() -> {
+
+            if (Optional.ofNullable(inFlight.get(serverName))
+                        .map(enums -> enums.contains(typeEnum))
+                        .orElse(false)) {
+
+                LOGGER.debug("尝试创建发送到节点 {} 的 {} 任务失败，上次的指令还未收到回复", serverName, typeEnum.name());
+                return false;
+            } else {
+                LOGGER.debug("尝试创建发送到节点 {} 的 {} 任务成功", serverName, typeEnum.name());
+
+                inFlight.compute(serverName, (s, enums) -> {
+                    if (enums == null) {
+                        enums = new HashSet<>();
+                    }
+                    enums.add(typeEnum);
+                    return enums;
+                });
+
+                return true;
+            }
+        });
+    }
+
+    /**
+     * 真正发送消息的方法，内置了重发机制
+     */
+    private void sendImpl(String serverName, Operation operation, Response response) {
+        ChannelManager.getInstance(ChannelType.COORDINATE).getChannel(serverName);
     }
 }
