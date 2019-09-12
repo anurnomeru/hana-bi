6b8e60eb76df79e9c8af50f2d5234b023b3257ed
anur <anurnomeru@163.com>
2019-09-12 11:31:40 +0800

    移除了原数据引擎（的架子）

--- src/main/java/com/anur/data/model/HanabiObject.kt
+++
@@ -1,9 +0,0 @@
-package com.anur.data.model
-
-import java.util.stream.Collector
-import java.util.stream.Collectors
-
-/**
- * Created by Anur IjuoKaruKas on 2019/8/8
- */
-class HanabiObject(val key: String, val value: String)
